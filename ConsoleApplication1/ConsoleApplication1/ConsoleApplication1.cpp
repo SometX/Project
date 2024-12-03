1bbafc5657b4d8aa883e08d3fc10860c4cfa0294
SometX <Someto@vk.com>
2024-12-03 15:05:36 +0700

    First_Patch

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+    unsigned int n;
+    cout << "Введите целое число n (0 <= n <= 31): ";
+    cin >> n;
+    unsigned int result = 1U << n; // 2^n через битовый сдвиг
+    cout << "Десятичное: " << result << endl;
+    cout << "Восьмеричное: " << oct << result << endl;
+    cout << "Шестнадцатеричное: " << hex << result << endl;
+    return 0;
+}
